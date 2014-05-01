3aceccd5292cd77c352f858a86401d7f4df2a8b0
Roman Plessl <roman.plessl@oetiker.ch>
2014-05-01 13:53:22 +0200

    changed pid file

--- bin/run.sh
+++ bin/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-#`dirname $0`/zimbra-manager.pl prefork --listen='http://*:13000'
+`dirname $0`/zimbra-manager.pl prefork --pid-file=/tmp/zimbra-manager-prefork.pid --listen='http://*:13000'
 
-`dirname $0`/zimbra-manager.pl daemon --listen='http://*:13000'
+# `dirname $0`/zimbra-manager.pl daemon --listen='http://*:13000'
